980a056b2fbb558d030250ff5af4b963e0e0079c
Andrew2M <andrei.m.milea@gmail.com>
2023-02-16 15:47:21 +0200

    Change production URL

---
+++ assets/OptionSelectService.ef7c4da7.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index.852f85f4.js";const n=({service:e})=>o("option",{value:e._id,children:e.name});export{n as O};
---
+++ assets/SpecialistServices.026a740c.js
@@ -0,0 +1,1 @@
+import{j as s,F as r,a as e,S as a}from"./index.852f85f4.js";const t=()=>s(r,{children:[e("h1",{children:"Selecte a service"}),e(a,{})]});export{t as default};
